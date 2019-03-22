f911544ac36ffad749fd395ebb8342061f16bcc9
Amit Srivastava <amit.srivastava@ascenaretail.com>
2019-03-22 15:59:58 +0530

    Chatbot code with updates
    Chatbot code with updates

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,3 +1,4 @@
+/*global chrome*/
 import React, { Component } from 'react'
 import { Container, ListGroup, ListGroupItem, Button, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import './Home.scss';
@@ -7,6 +8,7 @@ import send from '../assets/send-icon.png';
 import axios from "axios";
 
 class HomePage extends Component {
+    el = React.createRef();
     constructor(props) {
         super(props);
         this.state = {
@@ -14,13 +16,19 @@ class HomePage extends Component {
             userMessageArray: [],
             message: '',
             isOpen: false,
+            messageDetail: {
+                user:'',
+                message:''
+            }
         }
         this.sendmessage = this.sendmessage.bind(this);
+        this.enterPressed = this.enterPressed.bind(this);
     };
-
+    
     //send message to the bot 
 
     sendmessage(e) {
+       
         e.preventDefault();
         axios.get('http://localhost:5003/api/getData', {
             params: {
@@ -35,12 +43,16 @@ class HomePage extends Component {
             this.state
         )
         this.state.message = '';
-    }
+    //     var list = document.getElementById("chat-group");
+    //     var targetLi = document.getElementById("chat-list"); // id tag of the <li> element
 
+    // list.scrollTop = (targetLi.offsetTop - 50);
+    }
+   
     //recieve message from the bot 
 
     botReplies(response) {
-        this.state.botMessageArray.push(response.data);
+        this.state.userMessageArray.push(response.data);
         this.setState(
             this.state
         )
@@ -52,8 +64,28 @@ class HomePage extends Component {
         this.setState({
             message: event.target.value
         })
+        
+        
+    }
+    enterPressed(event){
+        if (event.keyCode == 13 || event.which == 13){
+           this.sendmessage(event)
+        }
     }
 
+    componentDidMount() {
+        this.scrollToBottom();
+      }
+    
+      componentDidUpdate() {
+        this.scrollToBottom();
+      }
+    
+      scrollToBottom() {
+        const node = this.refs.el;
+        node && node.scrollIntoView({block: "end", behavior: 'smooth'})
+        // this.el.scrollIntoView({ behavior: 'smooth' });
+      }
     //render the HTML part
 
     render() {
@@ -67,24 +99,24 @@ class HomePage extends Component {
                                 <span className="chat-label">Ask Josie</span>
                                 <span className="float-right msg-icon-container"><img src={chatIcon} width='25px' height='22px' className="message-icon" ></img></span>
                             </div>
-                            <div>
-                                <ListGroup>
+                            <div  ref={el => { this.el = el; }}>
+                                <ListGroup id="chat-group">
                                     {
-                                        this.state.botMessageArray.map(data => <ListGroupItem className="bot-Message" key={data}> {data} </ListGroupItem>)
+                                        this.state.botMessageArray.map(data => <ListGroupItem id="chat-list"  className="bot-Message" key={data}> {data} </ListGroupItem>)
                                     }
                                     {
-                                        this.state.userMessageArray.map(item => <ListGroupItem className="user-Message" key={item}> {item} </ListGroupItem>)
+                                        this.state.userMessageArray.map(item => <ListGroupItem id="chat-list" className="user-Message" key={item}> {item} </ListGroupItem>)
                                     }
                                 </ListGroup>
                             </div>
                             <div className="input-group msg-box">
-                                <input id="textbox" type="text" className="form-control" name="textbox" value={this.state.message}
-                                    onChange={(event) => this.inputChangedHandler(event)} />
-                                <span onClick={this.sendmessage} className="send-icon"> <img src={send} width='20px' height='20px' /></span>
+                                <input id="textbox" type="text" className="form-control type-textbox" name="textbox" value={this.state.message}
+                                    onChange={(event) => this.inputChangedHandler(event)} autoComplete="off" onKeyPress= {this.enterPressed} />
+                                <span onClick={this.sendmessage} className="send-icon"> Chat<img src={send} width='20px' height='20px' /></span>
                             </div>
                         </div>
                         <div>
-                           
+
                         </div>
                     </div>
 
